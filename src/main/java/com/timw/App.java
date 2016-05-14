fb565b9bbaaa956679dc15d1bcb536868a83f236
timwebster9 <kife1234>
2016-05-14 14:02:52 +0100

    Initial commit

---
+++ src/main/java/com/timw/App.java
@@ -0,0 +1,13 @@
+package com.timw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
